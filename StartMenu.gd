aa62a06817b819872cb0e963b20d4779af06ee3f
Fabian1 <60055343+fabianlanza@users.noreply.github.com>
2022-02-08 10:42:40 -0600

    Added Main Menu

---
+++ StartMenu.gd
@@ -0,0 +1,12 @@
+#Start Menu
+extends Control
+
+
+#Gets signal from button Start
+func _on_StartGameButton_pressed():
+	get_tree().change_scene("res://mainscene.tscn") #open main scene
+	
+
+#Gets signal from button Quit
+func _on_QuitGameButton_pressed():
+	get_tree().quit()#quit
